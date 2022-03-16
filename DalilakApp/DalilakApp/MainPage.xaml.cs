9bd0ac8e5e7287bad9fe7de348c06da44870655c
Omar Ayoub <omar-kr@outlook.sa>
2022-03-17 01:05:49 +0300

    1:05 - 3/17/2022

--- DalilakApp/DalilakApp/MainPage.xaml.cs
+++ DalilakApp/DalilakApp/MainPage.xaml.cs
@@ -7,28 +7,39 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Newtonsoft.Json;
-
+using System.IO;
 
 namespace DalilakApp
 {
     public partial class MainPage : ContentPage
     {
         private string loginTxt;
+        private Services.DalilakapiService api = new Services.DalilakapiService();
+
         public MainPage()
         {
             InitializeComponent();
+
+            // Function to disaply one image for Al-Hada
+            displayImages();
         }
 
         private async void btn_login_Clicked(object sender, EventArgs e)
         {
-            loginTxt = await DisplayPromptAsync("Enter your phone number", "+966");
-            Services.DalilakapiService api = new Services.DalilakapiService();
+            //loginTxt = await DisplayPromptAsync("Enter your phone number", "+966");
+            //Services.DalilakapiService api = new Services.DalilakapiService();
             
-            if(await api.login(loginTxt) == true)
-            {
-                btn_login.Text = "test";
-            }
+            //if(await api.login(loginTxt) == true)
+            //{
+            //    btn_login.Text = "test";
+            //}
 
+        }
+
+        private async void displayImages()
+        {
+            byte[] Base64Stream = Convert.FromBase64String(await api.image("b279738fb9a444e49c69173a9379c137"));
+            img.Source = ImageSource.FromStream(() => new MemoryStream(Base64Stream));
         }
     }
 }
--- DalilakApp/DalilakApp/Services/DalilakapiService.cs
+++ DalilakApp/DalilakApp/Services/DalilakapiService.cs
@@ -8,18 +8,37 @@ namespace DalilakApp.Services
 {
     public class DalilakapiService
     {
-        public async Task<bool> login(string phone)
+        private HttpClient client = new HttpClient();
+        private HttpResponseMessage response;
+        private Uri uri = null;
+        public async Task<string> login(string phone)
         {
-            HttpClient client = new HttpClient();
-            Uri uri = new Uri("http://api.dalilak.pro/Login/user_?phone="+phone);
+            uri = new Uri("http://api.dalilak.pro/Login/user_?phone="+phone);
 
-            HttpResponseMessage response = await client.PostAsync(uri, null);
+            response = await client.PostAsync(uri, null);
             
-           // if (response.IsSuccessStatusCode)
-            //{
-               return Convert.ToBoolean(response.Content.ReadAsStringAsync().Result.ToString());
-            //}
-            //return null;
-}
+            uri = null;
+            if (response.IsSuccessStatusCode)
+            {
+               return response.Content.ReadAsStringAsync().Result.ToString();
+            }
+            return null;
+        }
+
+        public async Task<string> image(string id)
+        {
+            
+            uri = new Uri("http://api.dalilak.pro/Query/PlaceImage_?place_id="+id);
+
+            response = await client.GetAsync(uri);
+
+            uri = null;
+
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsStringAsync().Result.ToString().Split(',')[1];
+            else
+                return null;
+        }
+        
     }
 }
