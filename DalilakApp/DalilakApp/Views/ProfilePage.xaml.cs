8adb267ab3efcfbeeecfec65f03b7cac0c62002d
Abdullah Alamri <aa505020@hotmail.com>
2022-04-05 16:50:50 +0300

    finish Button edid 04:49 04-09-1443

--- DalilakApp/DalilakApp/Views/ProfilePage.xaml.cs
+++ DalilakApp/DalilakApp/Views/ProfilePage.xaml.cs
@@ -24,10 +24,10 @@ namespace DalilakApp.Views
             
             email.Text = user.email;
             phone_num.Text =user.phone_num;
-            name.Placeholder = user.name;
-            age.Text +=  user.age;
-            information.Placeholder =  user.information;
-            city.Placeholder = user.city_id;
+            name.Text = user.name;
+            age.Text = Convert.ToString(this.user.age) ;
+            information.Text =  user.information;
+            city.Text = user.city_id;
         }
         private async void displayImage()
         {
@@ -44,14 +44,57 @@ namespace DalilakApp.Views
             Navigation.PushAsync(new RecordPage(user.id));
         }
 
-        private async void btn_edit_Clicked(object sender, EventArgs e)
+        private async void btn_ok_Clicked(object sender, EventArgs e)
         {
+             
+
+            if (int.TryParse(Eage.Text, out int n))
+            {
+                Boolean state = await api.UpdateUser(user.id, Ename.Text, Convert.ToInt32(this.Eage.Text), Einformation.Text, Ecity.Text);
+                if (state)
+                {
+                    await DisplayAlert("Note", " The data has been modified  ", "OK");
+                    btn_edit.IsVisible = true;
+                    btn_history.IsVisible = true;
+                    btn_favorit.IsVisible = true;
+                    noEdit.IsVisible = true;
+                    btn_ok.IsVisible = false;
+                    Edit.IsVisible = false;
+                }
+                else
+                {
+                    await DisplayAlert("ERROR", " Please verify the data entered ", "OK");
+                }
+            }
+            else
+                await DisplayAlert("ERROR", " Please verify the data entered ", "OK");
+            
+
+
+            
+        }
+        private  void btn_edit_Clicked(object sender, EventArgs e)
+        {
+            noEdit.IsVisible = false;
+            Edit.IsVisible = true;
+            Ename.Placeholder = user.name;
+            Eage.Placeholder += user.age;
+            Einformation.Placeholder = user.information;
+            Ecity.Placeholder = user.city_id;
+            btn_edit.IsVisible = false;
+            btn_history.IsVisible = false;
+            btn_favorit.IsVisible = false;
+            btn_ok.IsVisible = true;
+
+            /*
             Boolean state = await api.UpdateUser(user.id, name.Text, Convert.ToInt32(this.age.Text), information.Text, city.Text);
             if (state)
             {
                 await DisplayAlert("Note", " The data has been modified  ", "OK");
             }else
                 await DisplayAlert("ERROR", " Please verify the data entered ", "OK");
+            */
+
         }
     }
 }
